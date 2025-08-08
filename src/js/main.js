53e94c1945ac006c34a721ce07e5e7aa1e1d1fd7
reza-foroutan <rezaforoutanjs@gmail.com>
2025-08-08 20:30:31 +0330

    version-000

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// header-top
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 8500,
+    disableOnInteraction: false,
+  },
+});
+// End header-top
